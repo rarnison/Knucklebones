c5d21f12ccd94c200f1c89e7a9b12651935cd7f8
Ross Arnison <arnisonr@gmail.com>
2024-03-14 23:29:49 +0000

    Added web container and implemented console version

---
+++ Knucklebones.WebApp/Versions/Console/CapturedInput.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace Knucklebones.WebApp.Versions.Console;
+
+public class CapturedInput : Knucklebones.Console.Screen.Input
+{
+    private static readonly ManualResetEvent PropertyChangedEvent = new(false);
+    private static ConsoleKeyInfo _keyInfo;
+
+    protected override ConsoleKeyInfo ProtectedReadKey()
+    {
+        PropertyChangedEvent.WaitOne();
+        PropertyChangedEvent.Reset();
+        return _keyInfo;
+    }
+
+    public void SendKey(KeyboardEventArgs args)
+    {
+        _keyInfo = new ConsoleKeyInfo(' ', MapKey(args.Key), args.ShiftKey, args.AltKey, args.CtrlKey);
+        PropertyChangedEvent.Set();
+    }
+
+    private static ConsoleKey MapKey(string key)
+    {
+        return key switch
+        {
+            "Enter" => ConsoleKey.Enter,
+            "ArrowLeft" => ConsoleKey.LeftArrow,
+            "ArrowRight" => ConsoleKey.RightArrow,
+            "a" => ConsoleKey.A,
+            "d" => ConsoleKey.D,
+            _ => ConsoleKey.NoName
+        };
+    }
+
+    public void Clear()
+    {
+        PropertyChangedEvent.Reset();
+    }
+}
---
+++ Knucklebones.WebApp/Versions/Console/ConsoleFriend.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Knucklebones.WebApp.Versions.Console;
+
+public static class ConsoleFriend
+{
+    public static void RedirectConsoleOut(Action<string> update)
+    {
+        System.Console.SetOut(new ConsoleOut(update));
+    }
+
+    private class ConsoleOut : TextWriter
+    {
+        private readonly Action<string> _update;
+        
+        public override Encoding Encoding => Encoding.Default;
+
+        public override void Write(string? value)
+        {
+            if (!string.IsNullOrEmpty(value?.Trim()))
+                _update(value);
+        }
+        
+        public ConsoleOut(Action<string> update)
+        {
+            _update = update;
+        }
+    }
+}
